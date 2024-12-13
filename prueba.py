90f238aa44466d4ab89d00cb41c2713aeee80f60
Lucas Sorzio <sorziolucas4@gmail.com>
2024-12-13 12:18:55 -0300

    pequeño repaso de objetos

---
+++ prueba.py
@@ -0,0 +1,25 @@
+class Auto:
+    pass
+
+
+class Auto:
+    def __init__(self,cantRueda,color):
+        self.cantRueda=cantRueda
+        self.color=color
+      
+       
+    def encendido(self):
+        print("run run run")
+       
+       
+       
+    
+
+
+    
+
+
+coche1= Auto("4 ruedas" ,"negro")  
+
+print(coche1.cantRueda) 
+coche1.encendido()
